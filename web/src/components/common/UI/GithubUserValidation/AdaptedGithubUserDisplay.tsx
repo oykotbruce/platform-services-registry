b5be54fc881cf6c0723435e494ed5781e501e0c6
w8896699 <billy.li@gov.bc.ca>
2021-10-28 17:35:56 -0700

    quick fix for creating bug

--- web/src/components/common/UI/GithubUserValidation/AdaptedGithubUserDisplay.tsx
+++ web/src/components/common/UI/GithubUserValidation/AdaptedGithubUserDisplay.tsx
@@ -5,7 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { githubIDSearchKeyword } from '../../../../redux/githubID/githubID.action';
 import {
   GithubIdBaseInterface,
-  GithubIDBaseState
+  GithubIDBaseState,
 } from '../../../../redux/githubID/githubID.reducer';
 import { selectAllPersona } from '../../../../redux/githubID/githubID.selector';
 import TextInput from '../TextInput';
@@ -62,8 +62,6 @@ const AdaptedGithubUserDisplay: React.FC<any> = (props) => {
       dispatchSearchGithubIDInput({ persona, position, inputValue });
     }
   }, [userFieldInputEvent.value, allPersona, persona, position, dispatchSearchGithubIDInput]);
-
-
 
   return (
     <>
